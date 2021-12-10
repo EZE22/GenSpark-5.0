f4d5801d3a8a1b30d4a2dddb428362bc43b3461f
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2021-12-10 07:51:03 -0500

    Notes for improvement.
    Please see the notes I added in your code. Good job!

--- DragonCave/src/com/company/Main.java
+++ DragonCave/src/com/company/Main.java
@@ -7,25 +7,27 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-    // Dragon Cave Introduction
+    // Dragon Cave Introduction.    // Try using tripple quoated strings. 
     String Intro =
-            "\nYou are in a land full of dragons. In front of you," +
-            "\nyou see two caves. In one cave, the dragon is friendly" +
-            "\nand will share his treasures with you. The other dragon" +
+            "\nYou are in a land full of dragons. In front of you," +         
+            "\nyou see two caves. In one cave, the dragon is friendly" +       
+            "\nand will share his treasures with you. The other dragon" +     
             "\nis greedy and hungry and will eat you on sight." +
             "\nWhich cave will you go into? (1 or 2)\n";
 
-    // Input choice 1 or 2
-        System.out.println(Intro);
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+    // Input choice 1 or 2     // Make sure you code is formatted correctly 
+        System.out.println(Intro);                      
+        Scanner input = new Scanner(System.in);          
+        System.out.println(input.nextInt());             
 
-    // The outcome
+    // The outcome     // Try using tripple quoated strings.
         String Ending =
                 "\nYou approach the cave..." +
                 "\nIt is dark and spooky..." +
                 "\nA large dragon jumps out in front of you! He opens his jaw and..." +
                 "\nGobbles you down in one bite!";
         System.out.println(Ending);
+	    
+     // Always close the scanner when you are done with it. 
     }
 }
